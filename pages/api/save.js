262c0c00bb493b89b80c159d728b163f66f4bdbe
Josias Aurel <ndjosiasaurel@gmail.com>
2021-12-23 13:53:47 +0000

    add function to handle saving noes

---
+++ pages/api/save.js
@@ -0,0 +1,23 @@
+
+const { Deta } = require("deta");
+
+const deta = Deta(process.env.NEXT_PUBLIC_DETA_PROJECT_KEY);
+
+const notes = deta.Base("notes");
+
+export default function saveNote(req, res) {
+    if (req.method === "POST") {
+        const { note, key } = req.body;
+
+        try {
+            notes.put({
+                note,
+                owner: key
+            });
+            res.json({status: "Success"});
+        } catch(err) { res.json({status: "Failed"}); }
+        return;
+    }
+
+    res.json("Save Function");
+}
