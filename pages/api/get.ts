90e799539f86b495d19d578bb3c34f7191e53357
JosiasAurel <ndjosiasaurel@gmail.com>
2022-02-05 17:14:43 +0100

    add api fn to fetch single buffer

---
+++ pages/api/get.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { Deta } from "deta";
+
+const deta = Deta(process.env.NEXT_PUBLIC_DETA_PROJECT_KEY);
+
+const buffers = deta.Base("buffers");
+
+export default async function fetchBuffer(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { bufferKey } = req.body;
+
+    try {
+      const fetchedBuffer = await buffers.get(bufferKey);
+
+      res.json(fetchedBuffer);
+    } catch(err) {
+      res.json({
+        status: "Failed"
+      });
+    }
+  }
+
+  return;
+}
