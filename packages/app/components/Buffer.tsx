543f22e1a10cf37b1aef0fd9c5e8adc4d3d620d3
JosiasAurel <ndjosiasaurel@gmail.com>
2022-07-26 09:20:56 +0100

    add control buttons to Buffer Card

--- packages/app/components/Buffer.tsx
+++ packages/app/components/Buffer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Card, useClipboard, Text, Code, Divider, Tag } from "@geist-ui/core";
+import { Card, useClipboard, Text, Code, Divider, Badge, Button } from "@geist-ui/core";
 import toast from "react-hot-toast";
 
 type Props = Partial<Buffer>;
@@ -12,28 +12,36 @@ const Buffer: React.FC<Props> = ({ content, date, type, isPublic }): JSX.Element
     <Card style={{
       minWidth: "300px",
       maxWidth: "300px",
-    }} onClick={_ => {
-      copy(content);
-      toast("Copied to clipboard", { icon: "📎" });
-    }}>
-      {
-        type === "text" ?
-          <Text style={{ overflow: "scroll" }}>
-            {content}
-          </Text>
-          :
-          <Text my={0}>
-            <Code classic={true} style={{ overflow: "scroll" }} block >
+    }} >
+      <div onClick={_ => {
+        copy(content);
+        toast("Copied to clipboard", { icon: "📎" });
+      }}>
+        {
+          type === "text" ?
+            <Text style={{ overflow: "scroll" }}>
               {content}
-            </Code>
-          </Text>
-      }
+            </Text>
+            :
+            <Text my={0}>
+              <Code classic={true} style={{ overflow: "scroll" }} block >
+                {content}
+              </Code>
+            </Text>
+        }
+      </div>
+
       <Divider />
       <span style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "center" }}>
-        {new Date(date).toDateString()}
-        <Tag type={isPublic ? "success" : "default"}>
+        <Badge type={isPublic ? "success" : "default"}>
           {isPublic ? "Public" : "Private"}
-        </Tag>
+        </Badge>
+        <Button auto scale={0.35}>
+          Edit
+        </Button>
+        <Button type="error" auto scale={0.35}>
+          Delete
+        </Button>
       </span>
     </Card>
   );
