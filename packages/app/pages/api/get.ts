f11076eb97fb6da04a0b6294c6d38bfc6488f251
JosiasAurel <ndjosiasaurel@gmail.com>
2022-07-27 10:30:14 +0100

    add: route to get unique buffer

---
+++ packages/app/pages/api/get.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function getBuffer(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    
+    // console.log(req.query);
+  try {
+        const buffer = await prisma.buffer.findUnique({
+            where: {
+                id: req.query.id as string
+            }
+        });
+
+    res.json({
+      status: true,
+      buffer
+    });
+  } catch (error) {
+    res.json({
+      status: false,
+    });
+  }
+  return prisma.$disconnect();
+}
