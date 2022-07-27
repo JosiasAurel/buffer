3faca8a8f002e3a37217d31645dae4a81b7ab87a
JosiasAurel <ndjosiasaurel@gmail.com>
2022-07-27 17:32:02 +0100

    use planetscale as database

--- packages/app/pages/docs.tsx
+++ packages/app/pages/docs.tsx
@@ -1,10 +1,38 @@
 import React from "react";
-import { Text } from "@geist-ui/core";
+import { Text, Badge } from "@geist-ui/core";
+import Link from "next/link";
 
 const DocsPage: React.FC = (): JSX.Element => {
     return (
         <div>
-            <h2>Welcome to the Docs</h2>
+            <header style={{
+                width: "100vw",
+                backdropFilter: "blur(5px)",
+                top: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Link href="/">
+                    <Badge style={{ margin: "1em" }}>Buffered.link</Badge>
+                </Link>
+            </header>
+            <Text h1 style={{ textAlign: "center" }}>
+                Buffered.link CLI Docs
+            </Text>
+
+            <main style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-evenly",
+                alignItems: "center"
+            }}>
+                <Text>
+                    This documentation contains information about how to set up and use the Buffered.link CLI.
+                    <br />
+                    These intructions assume you are on a windows operating system but should be similar for other operating systems.
+                </Text>
+            </main>
         </div>
     )
 }
