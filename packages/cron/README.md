c293ceabb3973dbc0c187c0adc2f31f96b60f68a
JosiasAurel <ndjosiasaurel@gmail.com>
2022-08-09 22:01:19 +0100

    fix: cleaning expired buffers

---
+++ packages/cron/README.md
@@ -0,0 +1,10 @@
+
+# Cleaner Cron
+
+This CRON service is in charge of cleaning expired buffers from the database.
+
+The CRON service to do this runs on Deta (makes a single request at the moment).
+
+The actual service handling the cleaning runs on Replit [here](https://replit.com/@JosiasAurel/BufferedCleaner#index.js)
+
+
--- packages/cron/index.js
+++ packages/cron/index.js
@@ -1,25 +1,10 @@
 
 const { app } = require("deta");
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
+const axios = require("axios");
 
 app.lib.cron(async _ => {
-
-    const buffers = await prisma.buffer.findMany();
-    
-    for (let buffer of buffers) {
-        const expiryDate = new Date(buffer.expiryDate);
-        const today = new Date();
-
-        if (today > expiryDate) {
-            await prisma.buffer.delete({
-                where: {
-                    id: buffer.id
-                }
-            })
-        }
-    }
+    axios("https://BufferedCleaner.josiasaurel.repl.co")
+        .then(r => console.log(r));
 });
 
 module.exports = app;
